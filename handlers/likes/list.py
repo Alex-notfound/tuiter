2a20627e49aca903bd0a9e71ba24194037d8916c
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2020-05-07 20:58:30 +0200

    Create likes/list
    Se ha implementado la funcionalidad que permite listar los likes de un tuit.

---
+++ handlers/likes/list.py
@@ -0,0 +1,31 @@
+import webapp2
+from webapp2_extras import jinja2
+
+from model.tuit import Tuit
+from model.like import Like
+from utilities import Utilities
+
+
+class ListLikesHandler(webapp2.RequestHandler):
+    def get(self):
+        usr, url_usr, userActual = Utilities.checkUser()
+
+        tuit = Tuit.getTuit(self.request)
+        likes = Like.getLikesByTuit(tuit.key)
+
+        values = {
+            "usr": usr,
+            "url_usr": url_usr,
+            "userActual": userActual,
+            "tuit": tuit,
+            "likes": likes,
+            "title": "Likes"
+        }
+
+        jinja = jinja2.get_jinja2(app=self.app)
+        self.response.write(jinja.render_template("list_likes.html", **values))
+
+
+app = webapp2.WSGIApplication([
+    ('/likes/list', ListLikesHandler)
+], debug=True)
