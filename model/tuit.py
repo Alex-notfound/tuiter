85c26ff40207cc9805a9f64b69aa323aece05481
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2020-05-07 17:25:44 +0200

    Update tuit.py
    Se ha modificado la entidad Tuit, añadiendo el atributo el atributo user para almacenar al creador del tuit.

--- model/tuit.py
+++ model/tuit.py
@@ -2,8 +2,10 @@
 
 from google.appengine.ext import ndb
 
+from user import User
+
 
 class Tuit(ndb.Model):
-
-    dateTime = ndb.DateTimeProperty(auto_now_add=True)
+    dateTime = ndb.DateTimeProperty(auto_now_add=True, indexed=True)
     text = ndb.StringProperty(required=True)
+    user = ndb.KeyProperty(kind=User)
